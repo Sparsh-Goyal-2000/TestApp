a2352fbef4b13ac0b4dea59f36460ade56d273b0
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-12-02 14:41:17 +0530

    Fix Action

--- app/controllers/subscription_controller.rb
+++ app/controllers/subscription_controller.rb
@@ -5,15 +5,7 @@ TestApp::Application.load_tasks
 class SubscriptionController < ApplicationController
 
   def alert_subscriber
-    # Rake::Task[:alert_subscribers].reenable
-    Subscription.find_each do |subscription|
-      if subscription.remaining_days.in?(REMINDING_DAYS)
-        puts "Alerting Subscribers of Subscription Id-#{subscription.id}"
-        SubscriptionMailer.renewal_reminder(subscription).deliver_later
-      end
-    end
-
-
+    Rake::Task[:alert_subscribers].reenable
     redirect_to root_path, notice: 'Subscribers Alerted'
   end
 
