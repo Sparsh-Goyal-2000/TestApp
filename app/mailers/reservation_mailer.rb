b73aacf3c085ef408e9c5548d5cd14b89c1f9e8d
Sparsh Goyal <2000sparshgoyal@gmail.com>
2021-12-03 16:07:42 +0530

    Add attachments

--- app/mailers/reservation_mailer.rb
+++ app/mailers/reservation_mailer.rb
@@ -4,6 +4,7 @@ class ReservationMailer < ApplicationMailer
   def confirmed(reservation)
     @reservation = reservation
     @line_items = reservation.line_items.includes(:product)
+    attachments.inline['wallpaper'] = File.read('app/assets/images/img_01.jpg')
     mail to: reservation.customer.email, subject: 'Reservation Details'
   end
 
